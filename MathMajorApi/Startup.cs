3f1b4c0276f60dac3912cd78da7c525b6b2fba01
Jeff Bolt <fryguy0033@gmail.com>
2021-05-29 21:23:31 -0500

    Update to .NET 5

--- MathMajorApi/Startup.cs
+++ MathMajorApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NSwag;
 using NSwag.AspNetCore;
@@ -27,7 +28,7 @@ namespace MathMajorApi
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddMvc(options => options.EnableEndpointRouting = false);
 
 			services.AddApiVersioning(options =>
 			{
@@ -67,7 +68,7 @@ namespace MathMajorApi
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IApplicationLifetime applicationLifetime)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
 		{
 			if (env.IsDevelopment() || env.IsStaging())
 			{
